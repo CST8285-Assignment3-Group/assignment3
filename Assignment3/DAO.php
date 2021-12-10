1afa1c3564a3a68ad74532789ebedefffcd5a3aa
myygith <88632307+meisgith@users.noreply.github.com>
2021-12-10 12:10:31 -0500

    Update DAO.php

--- Assignment3/DAO.php
+++ Assignment3/DAO.php
@@ -4,15 +4,18 @@
 //create function that gets the list of the product categories 
 function get_product_categories($connection){
 
+//execute multi query
 $query = "select * from Catalog_categories";
 return $connection->query($query);
 }
 
+//execute query for produce code, name and price dpending on product category 
 function get_products($connection, $category){
 $query = "select p.product_code, p.product_name, p.product_price from Catalog_products p join  Catalog_categories c on p.category_id=c.id where c.category_name='".$category."'";
 return $connection->query($query);
 }
 
+//save the product information
 function save_product($connection, $code, $name, $price,$category_id){
 $query = 'insert into Catalog_products(product_code,product_name,product_price, category_id) values(\''.$code.'\',\''.$name.'\','.$price.','.$category_id.')';
 return $connection->query($query);
